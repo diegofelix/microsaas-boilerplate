b779306aae27298f32572507731f6084b34f8518
Diego Felix <diegoflx.oliveira@gmail.com>
2024-01-21 13:05:41 -0300

    feat: remove unused method

--- Battleroad/Championship/Models/Championship.php
+++ Battleroad/Championship/Models/Championship.php
@@ -7,15 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class Championship extends Model
 {
-    public static function fromRequest(RegisterNewChampionship $request): self
-    {
-        return new self([
-            'userId' => $request->user()->id,
-            'name' => $request->name,
-            'description' => $request->description,
-            'location' => $request->location,
-            'eventStart' => $request->eventStart,
-            'image' => $request->image,
-        ]);
-    }
 }
