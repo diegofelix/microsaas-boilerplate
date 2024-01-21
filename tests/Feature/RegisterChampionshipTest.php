eec62b6d925ebd856a578606b1bba6bb20e0d33f
Diego Felix <diegoflx.oliveira@gmail.com>
2024-01-21 13:41:13 -0300

    feat: add some tests for registering a new championship

--- tests/Feature/RegisterChampionshipTest.php
+++ tests/Feature/RegisterChampionshipTest.php
@@ -6,11 +6,40 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use function Pest\Laravel\{actingAs};
+use function Pest\Laravel\{postJson};
 
 it('shows validation errors', function () {
     $user = User::factory()->create();
 
     actingAs($user)
-        ->post('api/v1/championships', ['some' => 'input'])
-        ->assertOk();
+        ->postJson('api/v1/championships', ['some' => 'input'])
+        ->assertJsonValidationErrors([
+            'title', 'description', 'location', 'startAt', 'picture'
+        ]);
+});
+
+it('cannot create a championship without being logged in', function () {
+    postJson('api/v1/championships', ['some' => 'input'])
+        ->assertUnauthorized();
+});
+
+it('can register a new championship', function () {
+    $user = User::factory()->create();
+
+    actingAs($user)
+        ->postJson('api/v1/championships', [
+            'title' => 'Capcom Cup',
+            'description' => 'The ultimate Fighting Game Championship',
+            'location' => 'Brazil',
+            'startAt' => (new DateTime('tomorrow midnight'))->format('Y-m-d'),
+            'picture' => 'https://cdn.some.url/picture.jpg',
+        ])
+        ->assertOk()
+        ->assertJsonFragment([
+            'title' => 'Capcom Cup',
+            'description' => 'The ultimate Fighting Game Championship',
+            'location' => 'Brazil',
+            'picture' => 'https://cdn.some.url/picture.jpg',
+            'startAt' => '2024-01-22 00:00:00',
+        ]);
 });
