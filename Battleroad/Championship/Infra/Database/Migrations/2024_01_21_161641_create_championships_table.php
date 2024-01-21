864bce75ec713738e55741d7ff02cf8ccd38e738
Diego Felix <diegoflx.oliveira@gmail.com>
2024-01-21 13:25:51 -0300

    feat: add migration to championship

---
+++ Battleroad/Championship/Infra/Database/Migrations/2024_01_21_161641_create_championships_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('championships', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('owner_id');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->string('location')->nullable();
+            $table->timestamp('start_at');
+            $table->string('picture');
+            $table->foreign('owner_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('championships');
+    }
+};
--- Battleroad/Championship/Infra/ServiceProvider.php
+++ Battleroad/Championship/Infra/ServiceProvider.php
@@ -9,5 +9,8 @@ class ServiceProvider extends IlluminateServiceProvider
     public function register()
     {
         $this->app->register(RouteServiceProvider::class);
+        $this->loadMigrationsFrom(
+            base_path('Battleroad/Championship/Infra/Database/Migrations')
+        );
     }
 }
