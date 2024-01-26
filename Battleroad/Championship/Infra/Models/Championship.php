ce5ad802be1631fd3e8c5846ed08d51153734ea1
Diego Felix <diegoflx.oliveira@gmail.com>
2024-01-26 00:57:39 -0300

    feat: convert dates before saving to database

--- Battleroad/Championship/Infra/Models/Championship.php
+++ Battleroad/Championship/Infra/Models/Championship.php
@@ -5,9 +5,18 @@ namespace Battleroad\Championship\Infra\Models;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Relations\EmbedsMany;
 
 class Championship extends Model
 {
+
+    /**
+     * Cast dates from UTCDateTime to DateTime and vice-versa.
+     *
+     * @var string[]
+     */
+    protected $casts = ['start_at' => 'datetime'];
+
     /**
      * As we have total control over what we are inputting to this method
      * We can avoid guard fields.
