f8e5183d870630efb31a5bba40fead475af55d2b
Diego Felix <diegoflx.oliveira@gmail.com>
2024-02-12 19:41:34 -0300

    feat: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
