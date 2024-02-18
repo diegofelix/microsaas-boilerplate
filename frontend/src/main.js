5a275638099969ec96e80139b5fad8cbefde68b8
Diego Felix <diegoflx.oliveira@gmail.com>
2024-02-18 12:47:36 -0300

    feat: add axios and start login page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import axios from "axios"
+
+// These configuration allows us to use the backend server
+// on the same domain as the frontend
+axios.defaults.baseURL = 'https://backend.battleroad.local'
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
 
 const app = createApp(App)
 app.use(router)
